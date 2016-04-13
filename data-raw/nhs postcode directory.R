6aecf2a14f99a1d500e6afb3763413f100acd4fd
tony-stone <tony.stone@sheffield.ac.uk>
2016-04-13 17:18:22 +0100

    needed more data from postcode directory

--- data-raw/nhs postcode directory.R
+++ data-raw/nhs postcode directory.R
@@ -1,9 +1,12 @@
 library(data.table)
 
-nhs_postcode_directory <- fread("data-raw/geography data/Lookups/HSCIC gridlink/gridall.csv", sep = ",", header = FALSE, select = c(2:4, 37:38), colClasses = "character", na.strings="")
-setnames(nhs_postcode_directory, c("postcode", "pc_start", "pc_end", "easting", "northing"))
+nhs_postcode_directory <- fread("data-raw/geography data/Lookups/HSCIC gridlink/gridall.csv", sep = ",", header = FALSE, select = c(2:4, 12, 37:38), colClasses = "character", na.strings="")
+setnames(nhs_postcode_directory, c("postcode", "pc_start", "pc_end", "positional_quality_indicator", "easting", "northing"))
 
-nhs_postcode_directory[, ':=' (easting = as.integer(easting),
+nhs_postcode_directory[positional_quality_indicator == " ", positional_quality_indicator := NA]
+
+nhs_postcode_directory[, ':=' (positional_quality_indicator = as.integer(positional_quality_indicator),
+  easting = as.integer(easting),
   northing = as.integer(northing))]
 
 save(nhs_postcode_directory, file = "data/nhs postcode directory.Rda", compress = "xz")
