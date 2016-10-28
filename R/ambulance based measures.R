cdced91e108fa19221214076b8f71b3cf173fdb5
tony-stone <tony.stone@sheffield.ac.uk>
2016-10-28 14:21:23 +0100

    More efficient coding. Fixed measure name typo.

--- R/ambulance based measures.R
+++ R/ambulance based measures.R
@@ -16,8 +16,9 @@ save_ambulance_red_calls_measures <- function() {
   amb_mean_times <- calc_ambulance_times_measures(amb_data_red_calls_in_ca, FALSE, TRUE)
   amb_mean_times_site <- calc_ambulance_times_measures(amb_data_red_calls_in_ca, FALSE, FALSE)
 
-  amb_mean_times_n <- calc_ambulance_times_measures(amb_data_red_calls_in_ca, TRUE, TRUE)[, c("measure", "group", "site_type", "relative_month", "diff_time_to_ed") := NULL]
-  amb_mean_times_site_n <- calc_ambulance_times_measures(amb_data_red_calls_in_ca, TRUE, FALSE)[, c("measure", "group", "site_type", "relative_month") := NULL]
+  amb_mean_times_n <- calc_ambulance_times_measures(amb_data_red_calls_in_ca, TRUE)
+  amb_mean_times_site_n <- collapseLsoas2Sites(amb_mean_times_n)[, c("measure", "group", "site_type", "relative_month") := NULL]
+  amb_mean_times_n[, c("measure", "group", "site_type", "relative_month", "diff_time_to_ed") := NULL]
 
   setnames(amb_mean_times_n, "value", "n")
   setnames(amb_mean_times_site_n, "value", "n")
@@ -32,8 +33,8 @@ save_ambulance_red_calls_measures <- function() {
 
   # Call volume based measures ------------------------------------------------
 
-  amb_red_calls_measure <- calc_ambulance_vol_measures(amb_data_red_calls_in_ca, TRUE)
-  amb_red_calls_site_measure <- calc_ambulance_vol_measures(amb_data_red_calls_in_ca, FALSE)
+  amb_red_calls_measure <- calc_ambulance_vol_measures(amb_data_red_calls_in_ca)
+  amb_red_calls_site_measure <- collapseLsoas2Sites(amb_red_calls_measure)
 
   # save
   save(amb_red_calls_measure, file = createMeasureFilename("ambulance red calls"), compress = "bzip2")
@@ -43,15 +44,10 @@ save_ambulance_red_calls_measures <- function() {
 
 
 
-calc_ambulance_vol_measures <- function(amb_calls_data, lsoa_level = TRUE) {
+calc_ambulance_vol_measures <- function(amb_calls_data) {
 
-  if(lsoa_level) {
-    amb_vols_geo_month <- amb_calls_data[, .(value = .N), by = .(lsoa, yearmonth)]
-    ht_vols_geo_month <- amb_calls_data[outcome_of_incident == "2", .(value = .N), by = .(lsoa, yearmonth)]
-  } else {
-    amb_vols_geo_month <- amb_calls_data[, .(value = .N), by = .(town, yearmonth)]
-    ht_vols_geo_month <- amb_calls_data[outcome_of_incident == "2", .(value = .N), by = .(town, yearmonth)]
-  }
+  amb_vols_geo_month <- amb_calls_data[, .(value = .N), by = .(lsoa, yearmonth)]
+  ht_vols_geo_month <- amb_calls_data[outcome_of_incident == "2", .(value = .N), by = .(lsoa, yearmonth)]
 
   amb_vols_geo_month[, sub_measure := "total"]
   ht_vols_geo_month[, sub_measure := "hospital transfers"]
@@ -62,7 +58,7 @@ calc_ambulance_vol_measures <- function(amb_calls_data, lsoa_level = TRUE) {
   amb_call_geo_month[, measure := "ambulance red calls"]
 
   # format
-  amb_calls_measure <- fillDataPoints(amb_call_geo_month, TRUE, lsoa_level)
+  amb_calls_measure <- fillDataPoints(amb_call_geo_month, TRUE, TRUE)
 
   return(amb_calls_measure)
 }
@@ -80,14 +76,9 @@ calc_ambulance_times_measures <- function(amb_times_data, counts = FALSE, lsoa_l
 
   if(counts) {
 
-    if(lsoa_level) {
-      amb_times_geo_month <- amb_times_data_long[!is.na(value), .(value = .N), by = .(lsoa, yearmonth, sub_measure)]
-    } else {
-      amb_times_geo_month <- amb_times_data_long[!is.na(value), .(value = .N), by = .(town, yearmonth, sub_measure)]
-    }
-
+    amb_times_geo_month <- amb_times_data_long[!is.na(value), .(value = .N), by = .(lsoa, yearmonth, sub_measure)]
     # add measure var
-    amb_times_geo_month[, measure := "ambulance mean times"]
+    amb_times_geo_month[, measure := "ambulance mean times n"]
 
   } else {
 
@@ -98,7 +89,7 @@ calc_ambulance_times_measures <- function(amb_times_data, counts = FALSE, lsoa_l
     }
 
     # add measure var
-    amb_times_geo_month[, measure := "ambulance mean times n"]
+    amb_times_geo_month[, measure := "ambulance mean times"]
   }
 
   # format
@@ -124,40 +115,38 @@ save_ambulance_green_calls_measures <- function() {
 
   # Non-conveyance measures -------------------------------------------------
 
-  amb_non_conveyance_measure <- calc_non_conveyance_measure(amb_data_green_calls_in_ca, TRUE)
-  amb_non_conveyance_site_measure <- calc_non_conveyance_measure(amb_data_green_calls_in_ca, FALSE)
+  amb_green_calls_data <- calc_green_calls_vols_measure(amb_data_green_calls_in_ca)
+  amb_green_calls_site_data <- collapseLsoas2Sites(amb_green_calls_data)
+
+  # add fraction sub_measures
+  amb_green_calls_measure <- addFractionSubmeasure(amb_green_calls_data, "green calls", "not conveyed green calls", "fraction not conveyed")
+  amb_green_calls_site_measure <- addFractionSubmeasure(amb_green_calls_site_data, "green calls", "not conveyed green calls", "fraction not conveyed")
 
   # save
-  save(amb_non_conveyance_measure, file = createMeasureFilename("ambulance non-conveyance"), compress = "bzip2")
-  save(amb_non_conveyance_site_measure, file = createMeasureFilename("ambulance non-conveyance", "site"))
+  save(amb_green_calls_measure, file = createMeasureFilename("ambulance green calls"), compress = "bzip2")
+  save(amb_green_calls_site_measure, file = createMeasureFilename("ambulance green calls", "site"))
 }
 
 
 
 
-calc_non_conveyance_measure <- function(amb_calls_data, lsoa_level = TRUE) {
+calc_green_calls_vols_measure <- function(amb_calls_data) {
 
-  if(lsoa_level) {
-    amb_vols_geo_month <- amb_calls_data[, .(value = .N), by = .(lsoa, yearmonth)]
-    nc_vols_geo_month <- amb_calls_data[outcome_of_incident == "3" | outcome_of_incident == "5", .(value = .N), by = .(lsoa, yearmonth)]
-  } else {
-    amb_vols_geo_month <- amb_calls_data[, .(value = .N), by = .(town, yearmonth)]
-    nc_vols_geo_month <- amb_calls_data[outcome_of_incident == "3" | outcome_of_incident == "5", .(value = .N), by = .(town, yearmonth)]
-  }
+  amb_vols_geo_month <- amb_calls_data[, .(value = .N), by = .(lsoa, yearmonth)]
+  nc_vols_geo_month <- amb_calls_data[outcome_of_incident == "3" | outcome_of_incident == "5", .(value = .N), by = .(lsoa, yearmonth)]
+  ht_vols_geo_month <- amb_calls_data[outcome_of_incident == "2", .(value = .N), by = .(lsoa, yearmonth)]
 
   amb_vols_geo_month[, sub_measure := "green calls"]
   nc_vols_geo_month[, sub_measure := "not conveyed green calls"]
+  ht_vols_geo_month[, sub_measure := "hospital transfers"]
 
-  amb_call_geo_month <- rbind(amb_vols_geo_month, nc_vols_geo_month)
+  amb_call_geo_month <- rbind(amb_vols_geo_month, nc_vols_geo_month, ht_vols_geo_month)
 
   # add measure var
-  amb_call_geo_month[, measure := "ambulance non-conveyance"]
+  amb_call_geo_month[, measure := "ambulance green calls"]
 
   # format
-  amb_call_geo_month_formatted <- fillDataPoints(amb_call_geo_month, TRUE, lsoa_level)
+  amb_call_geo_month_formatted <- fillDataPoints(amb_call_geo_month, TRUE, TRUE)
 
-  # add fraction
-  amb_nonconveyed_measure <- addFractionSubmeasure(amb_call_geo_month_formatted, "green calls", "not conveyed green calls", "fraction not conveyed")
-
-  return(amb_nonconveyed_measure)
+  return(amb_call_geo_month_formatted)
 }
