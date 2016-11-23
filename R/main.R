1e8fadee5df13c72a2b25ac84dc82df956bc2e42
tony-stone <tony.stone@sheffield.ac.uk>
2016-11-23 16:56:08 +0000

    updated running time estimates

--- R/main.R
+++ R/main.R
@@ -5,25 +5,23 @@ genClosedMeasures <- function() {
   prepare_relevant_attendances()
   prepare_relevant_admitted_care()
 
-  # A&E based measures, 5.5mins
+  # A&E based measures, 7mins
   save_ed_attendances_measure()
   save_unnecessary_ed_attendances_measure()
   save_ed_attendances_admitted_measure()
 
-  # APC based measures, 10.5mins
+  # APC based measures, 9mins
   save_emergency_admissions_measure()
   save_critical_care_stays_measure()
   save_hospital_transfers_measure()
   save_length_of_stay_measure()
 
-  # Deaths based measures, ??mins
+  # Deaths based measures, 22mins
   link_hes_apc_and_death_data()
   save_deaths_measure()
   save_case_fatality_measure()
 
-  #Ambulance based measures, 4.5mins
-  pc <- proc.time()
+  #Ambulance based measures, 5mins
   save_ambulance_red_calls_measures()
   save_ambulance_green_calls_measures()
-  proc.time() - pc
 }
