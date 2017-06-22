7c8fe025a522eed26adaf1bb09918b17a4a26773
tony-stone <tony.stone@sheffield.ac.uk>
2017-06-22 17:31:39 +0100

    Additional plots for report and added pack to packrat

---
+++ R/catchment area population by time to next nearest ed.R
@@ -0,0 +1,33 @@
+library(data.table)
+library(ggplot2)
+
+load("data/catchment area set final.Rda")
+load("data/2001-census lsoa annual population estimates 2006-2014.Rda")
+
+intervention_sites <- catchment_area_set_final[site_type == "intervention", .(lsoa, town, group, diff_first_second, ae_post_intv, intervention_year = as.integer(format(intervention_date, "%Y")))]
+
+intervention_sites_populations_lsoa <- merge(intervention_sites, lsoa_population_annual_data, by.x = c("lsoa", "intervention_year"), by.y = c("LSOA01", "year"), all.x = TRUE)
+
+intervention_sites_populations <- intervention_sites_populations_lsoa[, .(population = sum(population)), by = .(town, group, diff_first_second, sex, age_cat)]
+
+by_site_plot <- ggplot(intervention_sites_populations_all, aes(x = diff_first_second, weight = population)) +
+  geom_histogram(aes(y = 1 * ..density..), position = 'identity', binwidth = 1) +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(breaks = 1:25, expand = c(0, 0.7)) +
+  coord_cartesian(xlim = c(1, 25)) +
+  labs(title = "Catchment area population by time to next nearest ED", x = "Time to next nearest ED\n(minutes)", y = "Catchment area population") +
+  theme_bw() +
+  facet_wrap(~group)
+
+
+
+sites_combined_plot <- ggplot(intervention_sites_populations_all, aes(x = diff_first_second, weight = population)) +
+  geom_histogram(aes(y = 1 * ..density..), position = 'identity', binwidth = 1) +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(breaks = 1:25, expand = c(0, 0.7)) +
+  coord_cartesian(xlim = c(1, 25)) +
+  labs(title = "All catchment areas population by time to next nearest ED", x = "Time to next nearest ED\n(minutes)", y = "Catchment areas population") +
+  theme_bw()
+
+ggsave("catchment area populations by time to next nearest ED - by site.png", by_site_plot, device = "png", path = "plots", width = 30, height = 20, units = "cm")
+ggsave("catchment area populations by time to next nearest ED - all sites combined.png", sites_combined_plot, device = "png", path = "plots", width = 30, height = 20, units = "cm")
